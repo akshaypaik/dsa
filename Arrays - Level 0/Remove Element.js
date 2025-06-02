26ddbcf1a3c4ad13c2aea1e56858206192209c14
Akshay Pai <akshaypaik@gmail.com>
2025-06-02 19:09:57 +0530

    removing element from array

---
+++ Arrays - Level 0/Remove Element.js
@@ -0,0 +1,23 @@
+// Leetcode problem link -> https://leetcode.com/problems/remove-element/
+
+const nums = [3, 1, 5, 4, 3, 4, 8, 3];
+
+const removeElements = (nums, val) => {
+
+    let x = 0;
+
+    for (let i = 0; i < nums.length; i++) {
+        if(nums[i] != val){
+            nums[x] = nums[i];
+            x++;
+        }
+    }
+
+    return x
+
+}
+
+console.log(removeElements(nums, 3));
+
+
+
