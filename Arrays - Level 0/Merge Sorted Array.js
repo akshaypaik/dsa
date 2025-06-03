2efd9d964dcd2e4989d8f467750a4c77bc3568b5
Akshay Pai <akshaypaik@gmail.com>
2025-06-03 12:58:45 +0530

    [ARRAY LEVEL 0]: merge sorted arrays

--- Arrays - Level 0/Merge Sorted Array.js
+++ Arrays - Level 0/Merge Sorted Array.js
@@ -1,3 +1,5 @@
+// Leetcode problem link -> https://leetcode.com/problems/merge-sorted-array
+
 const nums1 = [1, 2, 3, 0, 0, 0], m = 3, nums2 = [2, 5, 6], n = 3;
 
 // using extra space(extra variable)
