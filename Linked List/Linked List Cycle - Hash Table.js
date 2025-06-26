3c4a494ea5a322d85c906082b302dafd1ce1243b
Akshay Pai <akshaypaik@gmail.com>
2025-06-26 19:04:00 +0530

    [LINKED LIST]: Linked List Cycle - Hash Table - Floyd's Algorithm

--- Linked List/Linked List Cycle - Hash Table.js
+++ Linked List/Linked List Cycle - Hash Table.js
@@ -2,22 +2,46 @@
  * @param {ListNode} head
  * @return {boolean}
  */
+// const hasCycle = function (head) {
+
+//     let curr = head;
+//     let elmSet = new Set();
+
+//     while (curr) {
+//         // do not check only the node value like elmSet.add(curr.val)
+//         if (elmSet.has(curr)) {
+//             return true;
+//         } else {
+//             // stores the node reference. do not check only the node value like elmSet.add(curr.val);
+//             elmSet.add(curr);
+//             curr = curr.next;
+//         }
+//     }
+
+//     return false;
+
+// };
+
+
+
+// Floyd's Algorithm
 const hasCycle = function (head) {
 
-    let curr = head;
-    let elmSet = new Set();
-
-    while (curr) {
-        // do not check only the node value like elmSet.add(curr.val)
-        if (elmSet.has(curr)) {
-            return true;
-        } else {
-            // stores the node reference. do not check only the node value like elmSet.add(curr.val);
-            elmSet.add(curr);
-            curr = curr.next;
-        }
+    if (!head) {
+        return false;
     }
 
-    return false;
+    let slow = head;
+    let fast = head.next;
+
+    while (slow != fast) {
+        if (fast == null || fast.next == null) {
+            return false;
+        }
+        slow = slow.next;
+        fast == fast.next.next;
+    }
+
+    return true;
 
 };
