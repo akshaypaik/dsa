07c5ae327c691a9fe862f4d68b76609ba9c0402b
Akshay Pai <akshaypaik@gmail.com>
2025-06-29 23:08:23 +0530

    [STRINGS LEVEL 0]: Longest Common Prefix

---
+++ Strings - Level 0/Longest Common Prefix.js
@@ -0,0 +1,27 @@
+// Leetcode link -> https://leetcode.com/problems/longest-common-prefix/description/
+
+const strs = ["flower", "flow", "flight"];
+
+/**
+ * @param {string[]} strs
+ * @return {string}
+ */
+var longestCommonPrefix = function (strs) {
+
+    let x = 0;
+
+    while (x < strs[0].length) {
+        let ch = strs[0][x];
+        for (let i = 1; i < strs.length; i++) {
+            if (ch != strs[i][x] || x === strs[i].length) {
+                return strs[0].substring(0, x);
+            }
+        }
+        ++x;
+    }
+
+    return strs[0];
+
+};
+
+console.log(longestCommonPrefix(strs));
