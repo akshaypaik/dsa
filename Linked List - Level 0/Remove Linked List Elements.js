d9c36ff92444eba5d1333873bf4f7b9f0e23eca2
Akshay Pai <akshaypaik@gmail.com>
2025-06-30 19:33:36 +0530

    [LINKED LIST LEVEL 0]: Remove Linked List Elements

---
+++ Linked List - Level 0/Remove Linked List Elements.js
@@ -0,0 +1,32 @@
+// Leetcode link -> https://leetcode.com/problems/remove-linked-list-elements/
+
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+/**
+ * @param {ListNode} head
+ * @param {number} val
+ * @return {ListNode}
+ */
+const removeElements = function (head, val) {
+
+    let sentinel = new ListNode();
+    sentinel.next = head;
+
+    let prev = sentinel;
+
+    while (prev && prev.next) {
+        if (prev.next.val === val) {
+            prev.next = prev.next.next;
+        } else {
+            prev = prev.next;
+        }
+    }
+
+    return sentinel.next;
+
+};
