456d85194b0a5209209da5927ed931b916e67ddb
Akshay Pai <akshaypaik@gmail.com>
2025-07-04 10:06:54 +0530

    [LINKED LIST LEVEL 0]: Remove Duplicates from Sorted List

---
+++ Linked List - Level 0/Remove Duplicates from Sorted List.js
@@ -0,0 +1,29 @@
+// Leetcode link -> https://leetcode.com/problems/remove-duplicates-from-sorted-list/description/
+
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+/**
+ * @param {ListNode} head
+ * @return {ListNode}
+ */
+const deleteDuplicates = function (head) {
+
+    let curr = head;
+
+    while (curr && curr.next) {
+
+        if (curr.val === curr.next.val) {
+            curr.next = curr.next.next;
+        } else {
+            curr = curr.next;
+        }
+    }
+
+    return head;
+
+};
