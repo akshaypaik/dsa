6a4510255a27d699b6081355e8113ad47f1ff5f5
Akshay Pai <akshaypaik@gmail.com>
2025-07-07 12:55:10 +0530

    [TWO POINTERS AND SLIDING WINDOW]: Find the Index of the First Occurrence in a String

---
+++ Two Pointers and Sliding Window/Find the Index of the First Occurrence in a String.js
@@ -0,0 +1,30 @@
+// Leetcode link -> https://leetcode.com/problems/find-the-index-of-the-first-occurrence-in-a-string/description/
+
+/**
+ * @param {string} haystack
+ * @param {string} needle
+ * @return {number}
+ */
+
+const haystack = "sadbutsad";
+const needle = "sad"
+
+var strStr = function (haystack, needle) {
+    let n = haystack.length;
+    let m = needle.length;
+
+    for (let i = 0; i <= n - m; i++) {
+        let j = 0;
+        for (j = 0; j < m; j++) {
+            if (haystack[i + j] !== needle[j]) {
+                break;
+            }
+        }
+        if (j === m) {
+            return i;
+        }
+
+    }
+
+    return -1;
+};
