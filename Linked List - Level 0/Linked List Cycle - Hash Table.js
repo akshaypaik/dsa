700046381958df5e8197c4fa1d426b9a87bf4b52
Akshay Pai <akshaypaik@gmail.com>
2025-10-14 16:33:22 +0530

    [LINKED LIST LEVEL 0]: LinkedList Cycle

--- Linked List - Level 0/Linked List Cycle - Hash Table.js
+++ Linked List - Level 0/Linked List Cycle - Hash Table.js
@@ -2,30 +2,30 @@
  * @param {ListNode} head
  * @return {boolean}
  */
-// const hasCycle = function (head) {
+const hasCycle = function (head) {
 
-//     let curr = head;
-//     let elmSet = new Set();
+    let curr = head;
+    let elmSet = new Set();
 
-//     while (curr) {
-//         // do not check only the node value like elmSet.add(curr.val)
-//         if (elmSet.has(curr)) {
-//             return true;
-//         } else {
-//             // stores the node reference. do not check only the node value like elmSet.add(curr.val);
-//             elmSet.add(curr);
-//             curr = curr.next;
-//         }
-//     }
+    while (curr) {
+        // do not check only the node value like elmSet.add(curr.val)
+        if (elmSet.has(curr)) {
+            return true;
+        } else {
+            // stores the node reference. do not check only the node value like elmSet.add(curr.val);
+            elmSet.add(curr);
+            curr = curr.next;
+        }
+    }
 
-//     return false;
+    return false;
 
-// };
+};
 
 
 
 // Floyd's Algorithm
-const hasCycle = function (head) {
+const hasCycleFloyd = function (head) {
 
     if (!head) {
         return false;
