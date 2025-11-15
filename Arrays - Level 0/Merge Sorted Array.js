ea74e8c563f8e063380c383a2441fe52a2bbd87d
Akshay Pai <akshaypaik@gmail.com>
2025-11-15 13:08:03 +0530

    [ARRAY LEVEL 0] : Merge Sorted Array New Approach

--- Arrays - Level 0/Merge Sorted Array.js
+++ Arrays - Level 0/Merge Sorted Array.js
@@ -58,3 +58,31 @@ const mergeSortedArrays = (nums1, nums2, m, n) => {
 }
 
 console.log(mergeSortedArrays(nums1, nums2, m, n));
+
+
+// One More Solution 
+var merge = function (nums1, m, nums2, n) {
+    let i = m - 1;
+    let j = n - 1;
+    let k = m + n - 1;
+
+    while (i >= 0 && j >= 0) {
+        if (nums1[i] > nums2[j]) {
+            nums1[k] = nums1[i];
+            --i;
+        } else {
+            nums1[k] = nums2[j];
+            --j;
+        }
+        --k;
+    }
+
+    while (j >= 0) {
+        nums1[k] = nums2[j];
+        --j;
+        --k;
+    }
+
+    return nums1;
+
+};
